5c346eec54310df659fae9650292b00faeb6e45d
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-31 20:50:33 +0430

    delete the extra things

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
-import Navbar from "./Navbar";
-import About from "./Routes/About";
-import Blogs from "./Routes/Blogs";
-import Contact from "./Routes/Contact";
-import Home from "./Routes/Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/blogs/:id">
-            <Blogs />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>Test</h1>
+    </div>
   );
 }
 
