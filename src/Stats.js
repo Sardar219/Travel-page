b313b2d1bad22877f83ebb920d0dc4fdcb8ad4f1
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-31 21:03:13 +0430

    add the Stats.js

---
+++ src/Stats.js
@@ -0,0 +1,11 @@
+const Stats = () => {
+  return (
+    <div className="Stats">
+      <footer>
+        <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      </footer>
+    </div>
+  );
+};
+
+export default Stats;
